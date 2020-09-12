c9b8cd1c6e26150c7389740a5d13e5abd24fce3d
aenriq <aenriq29@asu.edu>
2020-09-12 16:40:05 -0700

    added inspiration page and changed link styling

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,22 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import LinkDisplay from '../components/LinkDisplay'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="container">
       <Head>
         <title>aenriq</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
         <div className={styles.header}>I'm{" "}  
-          <a href="localhost:3000">
-            Antonio
-          </a>
+          <LinkDisplay url='\'>Antonio</LinkDisplay>
         </div>
         <div className={styles.home_description}>
-          I am a third year computer science student at Arizona State University. I enjoy{" "} <a href="">
-            reading
-          </a>
-           about computers and software.
+          I am a third year computer science student at Arizona State University. I enjoy browsing aesthetically designed{" "} 
+          <LinkDisplay url='/inspiration'>websites</LinkDisplay>
+          {" "}for inspiration, as I am passionate about minimalist web design.
         </div>
       </main>
     </div>  
---
+++ pages/inspiration.js
@@ -0,0 +1,44 @@
+import ReadingData from '../data/readingPosts.json'
+import listStyles from '../styles/list.module.css'
+export default function inspiration()
+{
+    return(
+    <>
+        <article className="container">
+            <ul>
+                {ReadingData.map((readingDetail, index) => {
+                    return(
+                        <li className={listStyles.listitem}>
+                            <a target="_blank" href={readingDetail.url}>
+                                <div className={listStyles.header}>
+                                    {readingDetail.title}
+                                </div>
+                                <div className={listStyles.description}>
+                                    {readingDetail.description}
+                                </div> 
+                            </a>
+                        </li>
+                    )
+                })}
+            </ul>    
+        </article>  
+        <style jsx>
+        {`
+            a {
+                outline: none;
+                text-decoration: none;
+                color: inherit;
+                position: relative;
+                z-index: 1;
+                white-space: nowrap;
+            }
+            
+            a:hover,
+            a:focus{
+                color:#727272;
+                transition: height 150ms ease-in-out;
+            }
+        `}   
+        </style>
+    </>)
+}
