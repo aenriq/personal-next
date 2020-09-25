7becf8e6e84da72c4c04a5b91eb0af7e3c326044
aenriq <aenriq29@asu.edu>
2020-09-25 16:55:43 -0700

    adding header

--- components/layout.js
+++ components/layout.js
@@ -8,10 +8,31 @@ export default function Layout({ children })
         <Head>
             <title>antonio enriquez</title>
         </Head>
-        <div className="nav-bar">
-            
-        </div>
 
+        <div className="container">
+            <div className="header">
+                <div>aenriq.com</div>
+                <div className="last-item">pages</div>
+            </div>
+            <main className="children">
+                {children}
+            </main>
+        </div>
+        <style jsx>
+        {`
+            .header{
+                margin-botton:50px;
+                display:flex;
+                justify-content: space-between
+                font-size:2.1vh;
+                width:40vw;
+            }
+
+            .last-item{
+                margin-left:auto;
+            }
+        `}
+        </style>
 
     </>)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,18 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import LinkDisplay from '../components/LinkDisplay'
+
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>aenriq</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.header}>I'm{" "}  
-          <LinkDisplay url='\'>Antonio</LinkDisplay>
-        </div>
         <div className={styles.home_description}>
-          I am a third year computer science student at Arizona State University. I enjoy browsing aesthetically designed{" "} 
-          <LinkDisplay url='/inspiration'>websites</LinkDisplay>
+          Hi I'm Antonio, a third year computer science student at Arizona State University. I enjoy browsing aesthetically designed{" "} 
+          <a href='/inspiration'>websites</a>
           {" "}for inspiration, as I am passionate about minimalist web design.
         </div>
       </main>
